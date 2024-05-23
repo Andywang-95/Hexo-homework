7aa62e5224c213c30dc23d10df97d6dceab1271c
CE-Andy <andy.wang@gigacomputing.com>
2024-05-23 17:40:37 +0800

    更改手機尺寸menu選單打開時，點選任意一處即可關閉

--- js/index.js
+++ js/index.js
@@ -2,13 +2,23 @@ let menu_btn = document.querySelector(".menu-btn");
 let close_btn = document.querySelector(".close-btn");
 let menu = document.querySelector(".menu");
 
-menu_btn.addEventListener('click', ()=>{
-    menu_btn.style.display = "none";
-    close_btn.style.display = "block";
-    menu.style.display = "block";
-    }, false);
-close_btn.addEventListener('click', ()=>{
-    menu_btn.style.display = "block";
-    close_btn.style.display = "none";
-    menu.style.display = "none";
-    }, false);
+
+document.addEventListener('click', ()=>{
+    // console.log('點擊body')
+    let close_btn_display = window.getComputedStyle(close_btn)['display']
+    if(close_btn_display == 'block'){
+        menu_btn.style.display = "block";
+        close_btn.style.display = "none";
+        menu.style.display = "none";
+    }    
+    }, true);
+    menu_btn.addEventListener('click', ()=>{
+        menu_btn.style.display = "none";
+        close_btn.style.display = "block";
+        menu.style.display = "block";
+        }, false);
+    // close_btn.addEventListener('click', ()=>{
+    //     menu_btn.style.display = "block";
+    //     close_btn.style.display = "none";
+    //     menu.style.display = "none";
+    //     }, false);

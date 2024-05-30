f1c70328dfbaaeefe725e8fd4978e42fec6ab380
Jiahe <1995andywang@gmail.com>
2024-05-30 21:45:10 +0800

    變更菜單按鈕js設定，添加監聽功能，表頭表尾樣式、功能同步至首頁對應檔案中

--- script/index.js
+++ script/index.js
@@ -1,24 +1,46 @@
-let menu_btn = document.querySelector(".menu-btn");
-let close_btn = document.querySelector(".close-btn");
-let menu = document.querySelector(".menu");
+const menu_btn = document.querySelector(".menu-btn");
+const close_btn = document.querySelector(".close-btn");
+const menu = document.querySelector(".menu");
+const mask = document.querySelector(".mask");
 
-
-document.addEventListener('click', ()=>{
-    // console.log('點擊body')
-    let close_btn_display = window.getComputedStyle(close_btn)['display']
-    if(close_btn_display == 'block'){
-        menu_btn.style.display = "block";
-        close_btn.style.display = "none";
-        menu.style.display = "none";
+// 初始化熒幕設定，依照熒幕寬度初始化按鈕及菜單樣式
+window_width = window.innerWidth
+if(window.innerWidth > 766){
+    menu_btn.classList.add("close_class");
+    close_btn.classList.add("close_class");
+    menu.classList.remove("close_class");
+}else{
+    menu_btn.classList.remove("close_class");
+    close_btn.classList.add("close_class");
+    menu.classList.add("close_class");
+}
+// 監聽熒幕寬度變化，依照熒幕寬度改變按鈕及菜單樣式
+window.addEventListener('resize', ()=>{
+    mask.classList.add("close_class");
+    if(window.innerWidth > 766){
+        menu_btn.classList.add("close_class");
+        close_btn.classList.add("close_class");
+        menu.classList.remove("close_class");
+    }else{
+        menu_btn.classList.remove("close_class");
+        close_btn.classList.add("close_class");
+        menu.classList.add("close_class");
+    }
+})
+// 監聽全局點擊事件，事件捕獲，在close_btn顯示時，點擊除menu外的任意位置關閉menu及close_btn，打開menu_btn
+document.addEventListener('click', e=>{
+    if(!close_btn.classList.contains('close_class') && !menu.contains(e.target)){
+        // console.log("enter");
+        menu_btn.classList.remove("close_class");
+        close_btn.classList.add("close_class");
+        mask.classList.add("close_class");
+        menu.classList.add("close_class");
     }    
     }, true);
-    menu_btn.addEventListener('click', ()=>{
-        menu_btn.style.display = "none";
-        close_btn.style.display = "block";
-        menu.style.display = "block";
-        }, false);
-    // close_btn.addEventListener('click', ()=>{
-    //     menu_btn.style.display = "block";
-    //     close_btn.style.display = "none";
-    //     menu.style.display = "none";
-    //     }, false);
+// 監聽menu_btn點擊事件，點擊後執行對應步驟
+menu_btn.addEventListener('click', ()=>{
+    menu_btn.classList.add("close_class");
+    close_btn.classList.remove("close_class");
+    mask.classList.remove("close_class");
+    menu.classList.remove("close_class");
+    }, false);
